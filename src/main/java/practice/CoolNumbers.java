5035d2c5b43703cf03312b65275a284b832c4948
Tatiana <t.khokholko@qiwi.com>
2023-10-17 11:53:20 +0300

    finished hw collections search

--- src/main/java/practice/CoolNumbers.java
+++ src/main/java/practice/CoolNumbers.java
@@ -1,30 +1,53 @@
 package practice;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.TreeSet;
 
 public class CoolNumbers {
+    static ArrayList<String> listOfNumbers = new ArrayList<>();
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        String[] letters = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
+        for (String letter : letters) {
+            for (int num = 0; num < 10; num++) {
+                for (int reg = 1; reg < 200; reg++) {
+                    String region = String.valueOf(reg);
+                    if (reg < 10) {
+                        region = "0" + region;
+                    }
+                    String number = String.format("%s%d%d%d%s%s%s", letter, num, num, num, letter, letter, region);
+                    listOfNumbers.add(number);
+                }
+            }
+            if (listOfNumbers.size() <= 2_000_000) {
+                listOfNumbers.addAll(listOfNumbers);
+            }
+        }
+        return listOfNumbers;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.contains(number)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        int searchNumber = Collections.binarySearch(sortedList, number);
+        return searchNumber >= 0;
     }
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        return treeSet.contains(number);
     }
-
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,5 +1,10 @@
 package practice;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Main {
     /*
     TODO:
@@ -9,6 +14,53 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        CoolNumbers.generateCoolNumbers();
+        System.out.println(CoolNumbers.listOfNumbers);
+        System.out.println(CoolNumbers.listOfNumbers.size());
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите интересующий номер:");
+        String searchNumber = scanner.nextLine();
+
+        long timeCounterForList = System.nanoTime();
+        if (CoolNumbers.bruteForceSearchInList(CoolNumbers.listOfNumbers, searchNumber)) {
+            printSuccess("Поиск перебором", timeCounterForList);
+        } else {
+            printFail("Поиск перебором", timeCounterForList);
+        }
+
+        long timeCounterForBinary = System.nanoTime();
+        Collections.sort(CoolNumbers.listOfNumbers);
+        if (CoolNumbers.binarySearchInList(CoolNumbers.listOfNumbers, searchNumber)) {
+            printSuccess("Бинарный поиск", timeCounterForBinary);
+        } else {
+            printFail("Бинарный поиск", timeCounterForBinary);
+        }
+
+        long timeCounterForHash = System.nanoTime();
+        HashSet<String> hashSet = new HashSet<>(CoolNumbers.listOfNumbers);
+        if (CoolNumbers.searchInHashSet(hashSet, searchNumber)) {
+            printSuccess("Поиск в HashSet", timeCounterForHash);
+        } else {
+            printFail("Поиск в HashSet", timeCounterForHash);
+        }
+
+        long timeCounterForTree = System.nanoTime();
+        TreeSet<String> treeSet = new TreeSet<>(CoolNumbers.listOfNumbers);
+        if (CoolNumbers.searchInTreeSet(treeSet, searchNumber)) {
+            printSuccess("Поиск в TreeSet", timeCounterForTree);
+        } else {
+            printFail("Поиск в TreeSet", timeCounterForTree);
+        }
+    }
+    
+    private static void printSuccess(String methodName, long timeCounter){
+        System.out.println(methodName + ": номер найден, поиск занял: "
+                + (System.nanoTime() - timeCounter) + " нс");
+    }
+
+    private static void printFail(String methodName, long timeCounter){
+        System.out.println(methodName + ": номер не найден, поиск занял: "
+                + (System.nanoTime() - timeCounter) + " нс");
     }
 }
